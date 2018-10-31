5abd03fedc2eee3746b4d3811ac8941d5a6ad6a2
5afs <42383539+5afs@users.noreply.github.com>
2018-10-31 08:45:18 -0700

    Added Random notes

--- notes/Basic Python Notes.py
+++ notes/Basic Python Notes.py
@@ -1,8 +1,9 @@
+'''
 print("Hello World!")
 print()
-# This is a comment. I can write whatever I want
-# here and it won't do anything about it.
-# It has no effect on the code.
+This is a comment. I can write whatever I want
+here and it won't do anything about it.
+It has no effect on the code.
 print()  # this adds extra spaces in the terminal
 print("This will print here. Notice the spacing.")
 
@@ -18,7 +19,7 @@ print(6 // 4)
 print(12 // 3)
 print(9 // 4)  # results in an integer (without decimals)
 
-# MORE MATH STUFF
+MORE MATH STUFF
 
 print(6 % 4)
 print(5 % 3)
@@ -33,17 +34,95 @@ car_mph = 0.01
 
 print("I have a car called %s. It's pretty nice." % car_name)
 
-# Input
-# name = input("What is your name? ")
-# print("Hello %s" % name)
+Input
+name = input("What is your name? ")
+print("Hello %s" % name)
 
-# auto comment - Ctrl + /
-# age = input("How old are you? ")
-# print("%s?! You belong in a museum." % age)
+auto comment - Ctrl + /
+age = input("How old are you? ")
+print("%s?! You belong in a museum." % age)
 
-# Hidden age
+Hidden age
 real_age = int(input("how old are you? >_"))
 hidden_age = real_age + 5
 print(hidden_age)
 print("%d is incredibly old. You are actually %d years old" % (hidden_age, real_age))
+'''
+
+'''
+This is a multi-line comment
+I can type anywhere here
+'''
+
+
+# functions
+def printHelloWorld():
+    print("Hello World!")
+
+
+printHelloWorld()
+
+# f(x) = 2x + 3
+
+
+def f(x):
+    print(2*x + 3)
+
+
+f(1)
+f(5)
+f(5000)
+
+# Loops
+for i in (1, 2, 3):
+    printHelloWorld()
+
+print()
+for i in range(3):
+    printHelloWorld()
+
+print()
+for i in range(10):
+    printHelloWorld()
+
+print()
+for i in range(5):  # Range starts at 0 and goes to 4
+    f(i)
+
+for i in range(5):
+    print(i**2)
+
+
+# While loops
+a = 0
+while a < 10:
+    print(a)
+    a += 1  # This is the same thing as a = a + 1
+
+'''
+Hints with oops:
+For loops - Use when you know EXACTLy how many iterations
+While loops - Use when you DON'T know how may iterations
+'''
+
+# Random numbers
+import random  # This should always be on line 1
+print(random.randint(0, 100))
+
+
+# Control statements
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    else:
+        return "F"
+
+
+print(grade_calc(79))
 
