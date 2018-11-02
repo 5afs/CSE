3ccde71e165565df498616d2a8776f0f9c47646c
5afs <42383539+5afs@users.noreply.github.com>
2018-11-02 08:44:45 -0700

    finish draft of guess game

--- notes/Delaney Kelly - Guess Game.py
+++ notes/Delaney Kelly - Guess Game.py
@@ -1,16 +1,14 @@
 import random
-answer = random.randint(0, 10)
-
+a = random.randint(1, 10)
 
 for i in (1, 2, 3, 4, 5):
-    def guessing_correct(guess):
-    if guess == answer:
-        return "Correct!"
-    elif guess > answer:
-        return "Guess lower"
-    elif guess < answer:
-        return "Guess higher"
-
-
-
+    g = int(input("You have five tries. Guess a number from one to ten: "))
+    if g >= a:
+        print("Guess lower: ")
+    elif g <= a:
+        print("Guess higher: ")
+    elif g == a:
+        print("You guessed it!")
+    if i == 5:
+        print("You lost :( ")
 
