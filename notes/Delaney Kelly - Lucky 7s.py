a4f77aed9698223e0dc94d9e69190b5b1ccb9368
5afs <42383539+5afs@users.noreply.github.com>
2018-11-08 08:42:36 -0800

    wrote more almost finished

--- notes/Delaney Kelly - Lucky 7s.py
+++ notes/Delaney Kelly - Lucky 7s.py
@@ -1,4 +1,20 @@
 import random
 money = 15
-d6_1 = random.randint(1, 6)
-d6_2 = random.randint(1, 6)
+broke = False
+round = 1
+
+print("Welcome to Lucky 7s!")
+
+while money > 0:
+    d6_1 = random.randint(1, 6)
+    d6_2 = random.randint(1, 6)
+    if d6_1 + d6_2 == 7:
+        money = money + 5
+        round += 1
+    elif d6_1 + d6_2 != 7:
+        money = money - 1
+        round += 1
+
+if money == 0:
+    broke = True
+    print("You're broke! You lasted for %d rounds." % round)
