8555f7c9cbe4504d8b974283112e12b4a2bff55a
5afs <42383539+5afs@users.noreply.github.com>
2019-01-08 08:44:09 -0800

    semester 2 notes started

---
+++ notes/Delaney Kelly - Semester 2 Notes.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+# Single Line Comment
+cars = 5
+driving = True
+print("I have %d cars" % cars)
+
+'''
+age = input("How old are you? ")
+print("You are %s years old!?!" % age)
+'''
+
+colors = ["blue", "green", "yellow", "red", "orange", "purple"]
+colors.append("teal")
+
+print(colors)
+colors.pop(0)
+print(colors)
