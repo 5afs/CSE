9ee256ebd80018c739416552c3d85025a2b0d47f
5afs <42383539+5afs@users.noreply.github.com>
2019-01-11 09:45:55 -0800

    hangman

--- notes/Delaney Kelly - Hangman.py
+++ notes/Delaney Kelly - Hangman.py
@@ -1,28 +1,46 @@
 import random
-words = ["shouldn't", "dwarf", "elephant", "banjo", "pixel", "unzip", "don't", "green", "mystery", "bagpipes", "water"]
+words = ["shouldn't", "dwarf", "elephant", "banjo", "pixel", "unzip", "don't", "green", "mystery",
+         "bagpipes", "water"]
 
 answer_word = random.choice(words)
 
 answer_letters = list(answer_word)
 blank_answer = []
+guessed_letters = []
 mistakes = 8
 
-for item in answer_letters:
-    # Make _s for letters in a new list
+for item in answer_letters:  # Make _s for letters in a new list
     current_index = answer_letters.index(item)
     blank_answer.insert(current_index, "_ ")
 
-print("The answer is %d letters long" % len(answer_letters))
+if "'" in answer_letters:
+    blank_answer[answer_letters.index("'")] = "'"
+
+print('The answer is %d letters long' % len(answer_letters))
 print("".join(blank_answer))  # print a joined list
 
-while mistakes > 0:
+while mistakes > 0:  # actually playing the game
     guess = input("Guess a letter: ")  # should go in the loop
-    # if guess == character in answer_letters:
+    if guess in answer_letters:
+        mistakes -= 1
+        guessed_letters.append(guess)
+        blank_answer[answer_letters.index(guess)] = guess
+        print("".join(blank_answer))
+    elif guess in guessed_letters:
+        print("You have already guessed this letter. ")
+    elif guess not in answer_letters:
+        print("Sorry, that letter is not in the word.")
+        mistakes += 1
+
+if mistakes == 0 and guess != answer_letters:
+    print("You lost! ")
 
 
 '''
 you get 8 guesses
 if guess == answer_letters:
+
+if 'at' in listOfStrings :  # check if letter is in list
 
 for character in answer_letters:
     if character(guess) == character(answer_letters):  # replace with a *
@@ -31,8 +49,3 @@ for character in answer_letters:
         answer_letters.insert(current_index, "*")
         print(answer_letters)
 '''
-
-
-
-
-
