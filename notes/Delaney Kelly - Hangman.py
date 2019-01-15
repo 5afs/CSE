6fafaf6f60af67ef83a3d014197938db4f8027ce
5afs <42383539+5afs@users.noreply.github.com>
2019-01-15 08:42:29 -0800

    hangman

--- notes/Delaney Kelly - Hangman.py
+++ notes/Delaney Kelly - Hangman.py
@@ -2,8 +2,8 @@ import random
 words = ["shouldn't", "dwarf", "elephant", "banjo", "pixel", "unzip", "don't", "green", "mystery",
          "bagpipes", "water"]
 
-answer_word = random.choice(words)
-
+# answer_word = random.choice(words)
+answer_word = 'pizza'  # DELETE this later
 answer_letters = list(answer_word)
 blank_answer = []
 guessed_letters = []
@@ -21,43 +21,35 @@ print("".join(blank_answer))  # print a joined list
 guess = ""
 
 while mistakes > 0:  # actually playing the game
-    # Build blank_answer
     guessed_letters.append(guess)
-    # Take input
     if blank_answer == answer_letters:  # won
         mistakes = 0
         print("Yay!, you won")
-    elif mistakes == 0 and guess != answer_letters:  # lost
-        print("You lost! ")  # switch won and lost?
+
     guess = input("Guess a letter: ")  # should go in the loop
+
     if guess in answer_letters and guess not in guessed_letters:
         guessed_letters.append(guess)
-        blank_answer[answer_letters.index(guess)] = guess
+
+        for i in range(len(answer_letters)):  # A for loop that goes through each index
+            if answer_letters[i] == guess:
+                blank_answer[i] = guess
         print("".join(blank_answer))
 
-    elif guess not in answer_letters and guess not in guessed_letters:  # haven't guessed, not correct
-        guessed_letters.append(guess)
-        print("That letter is not in the word.")
     elif guess in guessed_letters:  # already guessed
         print("You have already guessed this letter.")
 
+    elif guess not in answer_letters and guess not in guessed_letters:  # haven't guessed before, not correct
+            if mistakes == 0 and blank_answer != answer_letters:  # lost
+                print("You lost! The answer was: %s" % answer_word)
+            else:
+                guessed_letters.append(guess)
+                mistakes -= 1
+                print("That letter is not in the word.")
+
+
+
 '''
-
-    elif blank_answer == answer_letters:  # won
-        mistakes = 0
-        print("Yay! You won!")
-        
-    
-
-you get 8 guesses
-if guess == answer_letters:
-
-if 'at' in listOfStrings :  # check if letter is in list
-
-for character in answer_letters:
-    if character(guess) == character(answer_letters):  # replace with a *
-        current_index = words.index(character)
-        answer_letters.pop(current_index)
-        answer_letters.insert(current_index, "*")
-        print(answer_letters)
+    Build blank_answer
+    Take input
 '''
