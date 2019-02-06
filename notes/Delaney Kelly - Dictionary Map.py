59daf46bb77d0fa55f3d3941720215b6dcb56b4a
5afs <42383539+5afs@users.noreply.github.com>
2019-02-06 08:42:21 -0800

    added more rooms

--- notes/Delaney Kelly - Dictionary Map.py
+++ notes/Delaney Kelly - Dictionary Map.py
@@ -1,19 +1,44 @@
 world_map = {
-    'R19A': {
-        'NAME': "Mr. Wiebe's Class",
-        'DESCRIPTION': "The classroom you are in right now. There are two exits to the north side.",
+    'CANDY_STORE': {
+        'NAME': "The Sweet Tooth Candy Store",
+        'DESCRIPTION': "There is a shelf with boxes of ferrero rocher chocolates right in front of you. "
+                       "They cost $18. You need to buy one. The door to the main alley south of you but to the west "
+                       "is another door. ",
         'PATHS': {
-            'NORTH': 'PARKING_LOT'
+            'SOUTH': 'ALLEY',
+            'WEST': 'DUMPSTERS_2'
         }
     },
-    'PARKING_LOT': {
-        'NAME': "The Parking Lot",
-        'DESCRIPTION': "There are cars parked here. To the south in Mr. Wiebe's room.",
+    'OFFICE_STORE': {
+        'NAME': "Office Depot",
+        'DESCRIPTION': "In front of you is a neatly stacked pile of boxes in all sizes. Large boxes are $6. "
+                       "There is also a shelf of pens and pencils, but they are all out of pencils. "
+                       "You need to find a pencil somewhere else. ",
         'PATHS': {
-            'SOUTH': 'R19A'
+            'SOUTH': 'ALLEY'
         }
-    }
+    },
+    'STATIONARY_STORE': {
+        'NAME': "The Stationary Store",
+        'DESCRIPTION': "There is a big wooden barrel filled with yellow pencils. They cost $4 each. ",
+        'PATHS': {
+            'SOUTHEAST': 'ALLEY',
+            'SOUTH': 'GATE_1'
+        }
+    },
+    'ALLEY': {
+        'NAME': "The dark alley",
+        'DESCRIPTION': "The entrance to under the bridge is south, behind you. In front of you, north, is a brick "
+                       "building with a sign that reads 'Office Depot'. To the left (northwest) is a stationary store. "
+                       "To the right (northeast) is a candy store.  ",
+        'PATHS': {
+            'NORTH': 'OFFICE_STORE',
+            'NORTHWEST': 'STATIONARY_STORE',
+            'NORTHEAST': 'CANDY_STORE'
+        }
+    },
 }
+
 
 # Other Variables
 current_node = world_map['R19A']  # starting room
