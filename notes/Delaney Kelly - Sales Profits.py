f095928397595ebc1044935e29a510f2d0182b78
5afs <42383539+5afs@users.noreply.github.com>
2019-05-09 08:45:19 -0700

    profits

--- notes/Delaney Kelly - Sales Profits.py
+++ notes/Delaney Kelly - Sales Profits.py
@@ -1,35 +1,24 @@
 import csv
 
-item_types = []
-item_profits = []
+different_items = {}
 
 with open("Sales Records (1).csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
     print("Processing...")
 
-    for row in reader:  # getting item types in a list
-        item_class = row[2]
-        if item_class not in item_types:
-            item_types.append(item_class)
+    for row in reader:
+        if row[0] == "Region":
+            continue
+        old_number = float(row[13])
+        item_type = row[2]
+        try:
+            different_items[item_type] += old_number
+        except KeyError:
+            different_items[item_type] = old_number
 
-    item_types.remove(item_types[0])
+    for value in different_items:
+       
 
-    for item in item_types:
-        item_profits.append(0)
-        # for row in reader:
-
-    print(item_types)
-    print(item_profits)
-
-'''
-make a list of ints for the profit sums
-for every row that has that item type, add the profit to the index for that item type
-find which item has the biggest profits
-print that item type out and its profit saying its the best one
-
-# item_profits[item_types.index(item)] = row[13]
-
-for row in reader:
-        for item in item_types:  # adding up the sums
-            item_profits.append(row[13])
-'''
+    print(different_items)
+    best_product = max(different_items)
+    print(best_product)
